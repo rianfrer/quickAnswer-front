0516715e3018f01d6ff28ee8f09577f1bf77b320
Rian Silva <rianfrer@gmail.com>
2022-09-20 11:08:42 -0300

    aplicaco de front com navbar completo

---
+++ src/pages/Agendamentos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Agendamentos() {
+    return (
+        <div className='agendamentos'>
+            <h1>Agendamentos</h1>
+        </div>
+    )
+}
+
+export default Agendamentos
---
+++ src/pages/Conversas.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Conversas() {
+    return (
+        <div className='conversas'>
+            <h1>Conversas</h1>
+        </div>
+    )
+}
+
+export default Conversas
