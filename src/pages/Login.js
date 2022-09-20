14ebf611a215e07ad44f96037785ca13dbec1d2f
Rian Silva <rianfrer@gmail.com>
2022-09-20 11:20:13 -0300

    Testanto branch

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+const { render } = require("react-dom");
+import React from 'react';
+
+import Time from './Time';
+import Partida from './Partida';
+
+export default class Login extends React.Component {
+
+}
