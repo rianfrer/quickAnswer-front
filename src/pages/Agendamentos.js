82c7e453010555f47f40289745cc7da62da75a69
Daniel Furtado <danielfurtadobezerra@gmail.com>
2022-09-29 18:16:06 -0300

    Primeiros passos da tela de Agendamento

--- src/pages/Agendamentos.js
+++ src/pages/Agendamentos.js
@@ -1,10 +1,56 @@
 import React from "react";
+import '../styles/Agendamentos.css';
+import { Table } from 'reactstrap';
+
 
 function Agendamentos() {
     return (
-        <div className='agendamentos'>
-            <h1>Agendamentos</h1>
-        </div>
+        <>
+            <div className="nav">
+                <Navbar>
+                    <Container>
+                        <Navbar.Brand> Navbar with text</Navbar.Brand>
+                        <Navbar.Collapse className="justify-content-end">
+                            <Navbar.Text>
+                                Signed in as: Mark Otto
+                            </Navbar.Text>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+            <div className="tabela">
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nome completo do cliente</th>
+                            <th>Horário agendado</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan={2}>Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </>
+
     )
 }
 
--- src/pages/Conversas.js
+++ src/pages/Conversas.js
@@ -4,14 +4,16 @@ import '../styles/Conversas.css';
 
 function Conversas() {
     return (
-        <div>
-            <ChatEngine
-                height="100vh"
-                projectID='9cef1784-6789-4e99-bf7b-3f4106620650'
-                userName='admin'
-                userSecret='1234'
-            />
-        </div>
+        <>
+            <div>
+                <ChatEngine
+                    height="100vh"
+                    projectID='9cef1784-6789-4e99-bf7b-3f4106620650'
+                    userName='admin'
+                    userSecret='1234'
+                />
+            </div>
+        </>
     )
 }
 
